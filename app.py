b5cb8941103e29356db9763ff2c5e607abbd094d
OleksandrShcherban <102971653+OleksandrShcherban@users.noreply.github.com>
2022-07-29 19:22:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///database.db")
-
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
 
 @app.after_request
 def after_request(response):
